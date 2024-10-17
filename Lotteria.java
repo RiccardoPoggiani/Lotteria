8239a8bac47357ebef02fbcec3013b5ad910e43e
RiccardoPoggiani <rikypoggiani06@gmail.com>
2024-10-17 08:32:38 +0200

    Update Lotteria.java

--- Lotteria.java
+++ Lotteria.java
@@ -5,32 +5,52 @@ import java.util.logging.Logger;
 
 public class Lotteria {
     public static void main(String[] args) {
-        System.out.println("Inizio lotteria");
-
-        // Istanza ed avvio del thread Estrazione
-        Estrazione e = new Estrazione();
-        e.start();  // Avvio del thread di estrazione
-
-        // Istanza di alcuni thread Giocatore
-        Giocatore g1 = new Giocatore(1, e);
-        Giocatore g2 = new Giocatore(2, e);
-        Giocatore g3 = new Giocatore(3, e);
-
-        // Avvio dei thread Giocatori
+        int n = 3;
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        Estrazione e = new Estrazione(n);
+        
+        System.out.println("Inizio lotteria...");
+        e.start();
+        
+        Giocatore g1 = new Giocatore(1, "a", e);
+        Giocatore g2 = new Giocatore(2, "b", e);
+        Giocatore g3 = new Giocatore(3, "c", e);
+        Giocatore g4 = new Giocatore(4, "d", e);
+      
         g1.start();
-        g2.start();
-        g3.start();
-
         try {
-            g1.join(); 
-            g2.join();
-            g3.join();
-        } catch (InterruptedException ex) {
+            g1.join();
+           } catch (InterruptedException ex) {
             Logger.getLogger(Lotteria.class.getName()).log(Level.SEVERE, null, ex);
-            System.err.println("Errore nel join!");
+        }
+        g2.start();
+        try {
+            g2.join();
+           } catch (InterruptedException ex) {
+            Logger.getLogger(Lotteria.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        g3.start();
+        try {
+            g3.join();
+           } catch (InterruptedException ex) {
+            Logger.getLogger(Lotteria.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        // Comunicazione fine gioco
-        System.out.println("Fine lotteria");
+        g4.start();
+        try {
+            g4.join();
+            } catch (InterruptedException ex) {
+            Logger.getLogger(Lotteria.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+          //istanza ed avvio del thread Estrazione
+           System.out.println("3° posto: " + e.vincitori[2]);
+           System.out.println("2° posto: " + e.vincitori[1]);
+           System.out.println("1° posto: " + e.vincitori[0]);
+           
+         System.out.println("Fine della lotteria...");
     }
 }
